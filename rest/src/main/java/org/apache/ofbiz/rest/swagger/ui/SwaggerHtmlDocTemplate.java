964aadc0b127bf40641e58cdfb7d1b543822cee8
Chatree Srichart <chatree.srichart@gmail.com>
2021-01-23 20:43:26 +0700

    Add icons to SwaggerHtmlDocTemplate

--- rest/src/main/java/org/apache/ofbiz/rest/swagger/ui/SwaggerHtmlDocTemplate.java
+++ rest/src/main/java/org/apache/ofbiz/rest/swagger/ui/SwaggerHtmlDocTemplate.java
@@ -1,9 +1,12 @@
 package org.apache.ofbiz.rest.swagger.ui;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.util.Base64;
 import org.apache.juneau.html.BasicHtmlDocTemplate;
 import org.apache.juneau.html.HtmlDocSerializerSession;
 import org.apache.juneau.html.HtmlWriter;
 import org.apache.ofbiz.base.util.FileUtil;
+
 
 /**
  * {@link org.apache.juneau.html.BasicHtmlDocTemplate}
@@ -14,6 +17,8 @@ public class SwaggerHtmlDocTemplate extends BasicHtmlDocTemplate {
 
     private static final String SWAGGER_UI_VERSION = "3.40.0";
     private static final String SWAGGER_UI_RESOURCES_ROOT = "component://rest/src/main/resources/org.apache.ofbiz.rest.swagger.ui/swagger-ui-" + SWAGGER_UI_VERSION + "/";
+    public static final String ICON_MIME_TYPE = "image/png";
+    public static final String ICON_DATA_URL_PREFIX = "data:" + ICON_MIME_TYPE + ";base64,";
 
     @Override
     protected void head(HtmlDocSerializerSession session, HtmlWriter w, Object o) throws Exception {
@@ -21,6 +26,22 @@ public class SwaggerHtmlDocTemplate extends BasicHtmlDocTemplate {
         w.nl(0);
         w.sTag("title").text("Swagger UI").eTag("title");
         w.nl(0);
+
+        String favicon32x32Data = Base64.encodeBase64String(FileUtils.readFileToByteArray(FileUtil.getFile(SWAGGER_UI_RESOURCES_ROOT + "favicon-32x32.png")));
+        w.oTag("link")
+                .attr("rel", "icon")
+                .attr("type", ICON_MIME_TYPE)
+                .attr("sizes", "32x32")
+                .attr("href", ICON_DATA_URL_PREFIX + favicon32x32Data)
+                .ceTag();
+
+        String favicon16x16Data = Base64.encodeBase64String(FileUtils.readFileToByteArray(FileUtil.getFile(SWAGGER_UI_RESOURCES_ROOT + "favicon-16x16.png")));
+        w.oTag("link")
+                .attr("rel", "icon")
+                .attr("type", ICON_MIME_TYPE)
+                .attr("sizes", "16x16")
+                .attr("href", ICON_DATA_URL_PREFIX + favicon16x16Data)
+                .ceTag();
 
         String swaggerUiCss = FileUtil.readString("UTF-8", FileUtil.getFile(SWAGGER_UI_RESOURCES_ROOT + "swagger-ui.css"));
         w.sTag("style").append(swaggerUiCss).eTag("style");
