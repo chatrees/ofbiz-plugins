b57fd7cdbcb445d7ad3d2785d851a0a78caf5657
Chatree Srichart <chatree.srichart@gmail.com>
2021-01-30 22:53:23 +0700

    Remove Http Method check from an operation handler

--- rest/src/main/java/org/apache/ofbiz/rest/operation/EntityOperationHandler.java
+++ rest/src/main/java/org/apache/ofbiz/rest/operation/EntityOperationHandler.java
@@ -1,6 +1,5 @@
 package org.apache.ofbiz.rest.operation;
 
-import org.apache.juneau.http.HttpMethod;
 import org.apache.juneau.http.exception.InternalServerError;
 import org.apache.juneau.http.exception.NotAcceptable;
 import org.apache.juneau.rest.RestContext;
@@ -32,7 +31,6 @@ public class EntityOperationHandler implements OperationHandler {
         Debug.logInfo("Entity: " + restContext.getRequest().getMethod() + " : " + restContext.getRequest().getPathInfo(), MODULE);
         RestRequest restRequest = restContext.getRequest();
         LocalDispatcher dispatcher = (LocalDispatcher) restRequest.getAttribute("dispatcher");
-        String method = restRequest.getMethod();
         Element entityElement = operation.getHandlerElement();
         String name = entityElement.getAttribute("name");
         String action = entityElement.getAttribute("action");
@@ -40,35 +38,33 @@ public class EntityOperationHandler implements OperationHandler {
             throw new NotAcceptable("The action is missing");
         }
 
-        if (HttpMethod.GET.equals(method)) {
-            if ("list".equals(action)) {
-                Map<String, Object> inputFields = new HashMap<>();
-                Map<String, Object> performFindListInMap = UtilMisc.toMap(
-                        "entityName", name,
-                        "inputFields", inputFields,
-                        "orderBy", null,
-                        "viewIndex", 0,
-                        "viewSize", 20,
-                        "noConditionFind", "Y"
-                );
-                try {
-                    Map<String, Object> performFindListResults = dispatcher.runSync("performFindList", performFindListInMap);
-                    Object list = performFindListResults.get("list");
-                    Object orderBy = performFindListInMap.get("orderBy");
-                    Object viewIndex = performFindListInMap.get("viewIndex");
-                    Object viewSize = performFindListInMap.get("viewSize");
-                    Object noConditionFind = performFindListInMap.get("noConditionFind");
-                    Object listSize = performFindListResults.get("listSize");
-                    return OperationResult.ok(list, UtilMisc.toMap(
-                            "X-ORDER-BY", orderBy != null ? String.valueOf(orderBy) : null,
-                            "X-VIEW-INDEX", viewIndex != null ? String.valueOf(viewIndex) : null,
-                            "X-VIEW-SIZE", viewSize != null ? String.valueOf(viewSize) : null,
-                            "X-NO-CONDITION-FIND", noConditionFind != null ? String.valueOf(noConditionFind) : null,
-                            "X-LIST-SIZE", listSize != null ? String.valueOf(listSize) : null
-                            ));
-                } catch (GenericServiceException e) {
-                    throw new InternalServerError(e);
-                }
+        if ("list".equals(action)) {
+            Map<String, Object> inputFields = new HashMap<>();
+            Map<String, Object> performFindListInMap = UtilMisc.toMap(
+                    "entityName", name,
+                    "inputFields", inputFields,
+                    "orderBy", null,
+                    "viewIndex", 0,
+                    "viewSize", 20,
+                    "noConditionFind", "Y"
+            );
+            try {
+                Map<String, Object> performFindListResults = dispatcher.runSync("performFindList", performFindListInMap);
+                Object list = performFindListResults.get("list");
+                Object orderBy = performFindListInMap.get("orderBy");
+                Object viewIndex = performFindListInMap.get("viewIndex");
+                Object viewSize = performFindListInMap.get("viewSize");
+                Object noConditionFind = performFindListInMap.get("noConditionFind");
+                Object listSize = performFindListResults.get("listSize");
+                return OperationResult.ok(list, UtilMisc.toMap(
+                        "X-ORDER-BY", orderBy != null ? String.valueOf(orderBy) : null,
+                        "X-VIEW-INDEX", viewIndex != null ? String.valueOf(viewIndex) : null,
+                        "X-VIEW-SIZE", viewSize != null ? String.valueOf(viewSize) : null,
+                        "X-NO-CONDITION-FIND", noConditionFind != null ? String.valueOf(noConditionFind) : null,
+                        "X-LIST-SIZE", listSize != null ? String.valueOf(listSize) : null
+                        ));
+            } catch (GenericServiceException e) {
+                throw new InternalServerError(e);
             }
         }
 
