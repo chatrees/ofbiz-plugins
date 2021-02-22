9ce095e9e6d860e12b07412b07a9f9f36897e4d4
Chatree Srichart <chatree.srichart@gmail.com>
2021-02-22 20:25:07 +0700

    Create testPatch() and testPut() test cases

--- rest/src/test/java/org/apache/ofbiz/rest/OperationTest.java
+++ rest/src/test/java/org/apache/ofbiz/rest/OperationTest.java
@@ -9,6 +9,7 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.juneau.json.JsonParser;
+import org.apache.juneau.marshall.Json;
 import org.apache.juneau.rest.client2.RestClient;
 import org.apache.juneau.rest.client2.RestClientBuilder;
 import org.apache.juneau.rest.client2.RestResponse;
@@ -19,9 +20,42 @@ import org.junit.*;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import java.util.HashMap;
 import java.util.Map;
 
 public class OperationTest {
+
+    private RestClient restClient;
+
+    @Before
+    public void setUp() throws Exception {
+        restClient = createAllSSLRestClient("https://localhost:8443/restexample/rest");
+    }
+
+    @Test
+    public void testGET() throws Exception {
+        RestResponse restResponse = restClient.get("examples/10082").run();
+        restResponse.assertStatus().code().is(200);
+        Map body = restResponse.getBody().parser(JsonParser.DEFAULT).as(Map.class);
+        String exampleId = (String) body.get("exampleId");
+        Assert.assertEquals("10082", exampleId);
+    }
+
+    @Test
+    public void testPatch() throws Exception {
+        Map<String, Object> body = new HashMap<>();
+        body.put("exampleName", "test 4");
+        RestResponse restResponse = restClient.patch("examples/10082", Json.DEFAULT.toString(body)).run();
+        restResponse.assertStatus().code().is(200);
+    }
+
+    @Test
+    public void testPut() throws Exception {
+        Map<String, Object> body = new HashMap<>();
+        body.put("exampleName", "test 5");
+        RestResponse restResponse = restClient.put("examples/10082", Json.DEFAULT.toString(body)).run();
+        restResponse.assertStatus().code().is(200);
+    }
 
     private RestClient createAllSSLRestClient(String rootUri) throws Exception {
         TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
@@ -55,19 +89,11 @@ public class OperationTest {
         RestClientBuilder restClientBuilder = MockRestClient.create()
                 .sslSocketFactory(sslConnectionFactory)
                 .connectionManager(ccm)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
                 .header("Authorization", "Basic YWRtaW46b2ZiaXo=")
                 .rootUri(rootUri);
 
         return restClientBuilder.build();
-    }
-
-    @Test
-    public void testGET() throws Exception {
-        RestClient restClient = createAllSSLRestClient("https://localhost:8443/restexample/rest");
-        RestResponse restResponse = restClient.get("examples/10082").run();
-        restResponse.assertStatus().code().is(200);
-        Map body = restResponse.getBody().parser(JsonParser.DEFAULT).as(Map.class);
-        String exampleId = (String) body.get("exampleId");
-        Assert.assertEquals("10082", exampleId);
     }
 }
