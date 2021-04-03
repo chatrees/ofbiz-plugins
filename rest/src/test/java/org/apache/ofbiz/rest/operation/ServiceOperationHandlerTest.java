cf504c0fb9bc61d3d4189123da7e5097f040e7ce
Chatree Srichart <chatree.srichart@gmail.com>
2021-04-03 14:18:43 +0700

    Mock rest request attributes in a Map

--- rest/src/test/java/org/apache/ofbiz/rest/operation/ServiceOperationHandlerTest.java
+++ rest/src/test/java/org/apache/ofbiz/rest/operation/ServiceOperationHandlerTest.java
@@ -15,9 +15,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 import org.w3c.dom.Element;
 
 import javax.servlet.ServletContext;
@@ -27,9 +25,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ServiceOperationHandlerTest {
@@ -65,15 +65,22 @@ public class ServiceOperationHandlerTest {
     private Map<String, Object> sessionAttributes;
     private Map<String, Object> httpServletRequestAttributes;
     private Map<String, Object> httpServletRequestParameters;
+    private Map<String, Object> restRequestAttributes;
+    private Map<String, Object> handlerElementAttributes;
 
     @Before
     public void setUp() throws Exception {
         servletContextAttributes = new HashMap<>();
         sessionAttributes = new HashMap<>();
-        httpServletRequestAttributes = new HashMap<>();
+        httpServletRequestAttributes = new ConcurrentHashMap<>();
         httpServletRequestParameters = new HashMap<>();
+        restRequestAttributes = new HashMap<>();
+        handlerElementAttributes = new HashMap<>();
 
         httpServletRequestAttributes.put("servletContext", servletContext);
+        restRequestAttributes.put("dispatcher", dispatcher);
+        handlerElementAttributes.put("mode", "sync");
+        handlerElementAttributes.put("name", "testService");
 
         when(servletContext.getAttributeNames()).thenReturn(Collections.enumeration(servletContextAttributes.keySet()));
         when(httpSession.getAttributeNames()).thenReturn(Collections.enumeration(sessionAttributes.keySet()));
@@ -87,18 +94,19 @@ public class ServiceOperationHandlerTest {
         when(dispatcher.runSync(anyString(), serviceContextCaptor.capture())).thenReturn(ServiceUtil.returnSuccess());
         when(dispatchContext.getModelService(anyString())).thenReturn(modelService);
 
-//        when(httpServletRequest.getAttribute("servletContext")).thenReturn(servletContext);
-        when(httpServletRequest.getAttribute(anyString())).then(new Answer<Object>() {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                String attrName = invocation.getArgument(0);
-                return httpServletRequestAttributes.get(attrName);
-            }
+        when(httpServletRequest.getAttribute(anyString())).then(invocation -> {
+            String attrName = invocation.getArgument(0);
+            return httpServletRequestAttributes.get(attrName);
         });
-        when(restRequest.getAttribute("dispatcher")).thenReturn(dispatcher);
+        when(restRequest.getAttribute(anyString())).then(invocation -> {
+            String attrName = invocation.getArgument(0);
+            return restRequestAttributes.get(attrName);
+        });
+        when(handlerElement.getAttribute(anyString())).then(invocation -> {
+            String attrName = invocation.getArgument(0);
+            return handlerElementAttributes.get(attrName);
+        });
         when(dispatcher.getDispatchContext()).thenReturn(dispatchContext);
-        when(handlerElement.getAttribute("mode")).thenReturn("sync");
-        when(handlerElement.getAttribute("name")).thenReturn("testService");
     }
 
     @Test
